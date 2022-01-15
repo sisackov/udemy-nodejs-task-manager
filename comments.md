25f5a7213a2753abbef71f5c394d5d65459e4e32
sisackov <1123889+sisackov@users.noreply.github.com>
2022-01-16 00:00:01 +0200

    section 10-7 done

---
+++ comments.md
@@ -0,0 +1,7 @@
+## run mongo in powershell
+
+```
+mongod --dbpath=/Users/sisac/mongodb-data
+```
+
+##
--- mongodb.js
+++ mongodb.js
@@ -17,9 +17,61 @@ MongoClient.connect(
 
         const db = client.db(databaseName);
 
-        db.collection('users').insertOne({
-            name: 'Stas',
-            age: 37,
-        });
+        // db.collection('users').insertOne(
+        //     {
+        //         name: 'Stas',
+        //         age: 37,
+        //     },
+        //     (error, result) => {
+        //         if (error) {
+        //             return console.log('Unable to insert user');
+        //         }
+        //         console.log(result.ops);
+        //     }
+        // );
+
+        // db.collection('users').insertMany(
+        //     [
+        //         {
+        //             name: 'Jen',
+        //             age: 28,
+        //         },
+        //         {
+        //             name: 'Gunther',
+        //             age: 27,
+        //         },
+        //     ],
+        //     (error, result) => {
+        //         if (error) {
+        //             return console.log('Unable to insert documents!');
+        //         }
+
+        //         console.log(result.ops);
+        //     }
+        // );
+
+        // db.collection('tasks').insertMany(
+        //     [
+        //         {
+        //             description: 'Clean the house',
+        //             completed: true,
+        //         },
+        //         {
+        //             description: 'Renew inspection',
+        //             completed: false,
+        //         },
+        //         {
+        //             description: 'Pot plants',
+        //             completed: false,
+        //         },
+        //     ],
+        //     (error, result) => {
+        //         if (error) {
+        //             return console.log('Unable to insert tasks!');
+        //         }
+
+        //         console.log(result.ops);
+        //     }
+        // );
     }
 );
