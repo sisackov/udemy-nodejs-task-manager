46e89b0cfb5271294f06035cba77bad3114b6734
sisackov <1123889+sisackov@users.noreply.github.com>
2022-01-19 13:56:48 +0200

    section 11-16 done

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -12,3 +12,18 @@ Task.findByIdAndDelete('5c1a63e8f0d4c50656c5ab28')
     .catch((e) => {
         console.log(e);
     });
+
+//same as above with async await
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count;
+};
+
+deleteTaskAndCount('5c1a634150c97706427d4661')
+    .then((count) => {
+        console.log(count);
+    })
+    .catch((e) => {
+        console.log(e);
+    });
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -12,3 +12,18 @@ User.findByIdAndUpdate('5c1a5a34d5a2ec046ca8f6bc', { age: 1 })
     .catch((e) => {
         console.log(e);
     });
+
+//same as above with async await
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count;
+};
+
+updateAgeAndCount('5c1a5a34d5a2ec046ca8f6bc', 2)
+    .then((count) => {
+        console.log(count);
+    })
+    .catch((e) => {
+        console.log(e);
+    });
