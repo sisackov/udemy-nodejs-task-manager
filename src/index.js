7bd84243441a9f1adc192c60ecf07f70357c9e59
sisackov <1123889+sisackov@users.noreply.github.com>
2022-01-24 14:49:17 +0200

    section 12 done

--- src/index.js
+++ src/index.js
@@ -214,3 +214,21 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+const Task = require('./models/task');
+const User = require('./models/user');
+
+const main = async () => {
+    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5c2e4dcb5eac678a23725b5b');
+    console.log(user);
+    if (!user) {
+        await user.populate('tasks').execPopulate();
+        console.log(user.tasks);
+    }
+};
+
+main();
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,83 +1,119 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
+
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.get('/users', async (req, res) => {
+router.post('/users/login', async (req, res) => {
     try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (error) {
-        res.status(500).send(error);
+        const user = await User.findByCredentials(
+            req.body.email,
+            req.body.password
+        );
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
+    } catch (e) {
+        res.status(400).send();
     }
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
+router.post('/users/logout', auth, async (req, res) => {
     try {
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+        await req.user.save();
+
+        res.send();
+    } catch (e) {
+        res.status(500).send();
     }
 });
 
-router.patch('/users/:id', async (req, res) => {
-    const _id = req.params.id;
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
 
-    //below is done because if a field does not exist in the collection, it will not throw an error.
-    //mongodb will just ignore it. this way we can verify if the field is valid and send an error if it is not
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
+});
+
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
-    const allowedUpdates = Object.keys(User.schema.paths).filter(
-        (field) => field.startsWith('_') === false
-    ); //this will return an array of all the fields in the schema that are not _id, _v, _c, etc.
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every((update) =>
         allowedUpdates.includes(update)
     );
+
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' });
     }
 
     try {
-        const user = await User.findByIdAndUpdate(_id, req.body, {
-            new: true, //this will return the updated user instead of the old one
-            runValidators: true, //this will run the validators on the updated user
-        });
-
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    } catch (error) {
-        res.status(400).send(error);
+        updates.forEach((update) => (req.user[update] = req.body[update]));
+        await req.user.save();
+        res.send(req.user);
+    } catch (e) {
+        res.status(400).send(e);
     }
 });
 
-router.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id;
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(_id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
+        await req.user.remove();
+        res.send(req.user);
+    } catch (e) {
+        res.status(500).send();
     }
 });
+
+// router.patch('/users/:id', async (req, res) => {
+//     const _id = req.params.id;
+
+//     //below is done because if a field does not exist in the collection, it will not throw an error.
+//     //mongodb will just ignore it. this way we can verify if the field is valid and send an error if it is not
+//     const updates = Object.keys(req.body);
+//     const allowedUpdates = Object.keys(User.schema.paths).filter(
+//         (field) => field.startsWith('_') === false
+//     ); //this will return an array of all the fields in the schema that are not _id, _v, _c, etc.
+//     const isValidOperation = updates.every((update) =>
+//         allowedUpdates.includes(update)
+//     );
+//     if (!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates!' });
+//     }
+
+//     try {
+//         const user = await User.findByIdAndUpdate(_id, req.body, {
+//             new: true, //this will return the updated user instead of the old one
+//             runValidators: true, //this will run the validators on the updated user
+//         });
+
+//         if (!user) {
+//             return res.status(404).send();
+//         }
+
+//         res.send(user);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// });
 
 module.exports = router;
